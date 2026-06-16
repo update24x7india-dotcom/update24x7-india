ae1e433ce1d563c8565143ae87ae5d03de87ba1a
update24x7india-dotcom <update24x7india@gmail.com>
2026-06-17 03:54:08 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'update24-pwa-v1';
+const CACHE_NAME = 'update24-pwa-v2';
 const FILES_TO_CACHE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
